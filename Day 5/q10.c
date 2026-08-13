8381feb05b26b96198307a5db2a6672f4accb9ec
Kanishk-Rana <kanishk.33995@stu.upes.ac.in>
2026-08-14 01:29:39 +0530

    Day 5

---
+++ Day 5/q10.c
@@ -0,0 +1,18 @@
+//Write a program to input time in seconds and convert it to hours:minutes:seconds format.
+#include <stdio.h>
+
+int main()
+{
+    int total, h, m, s;
+
+    printf("enter time in seconds: ");
+    scanf("%d", &total);
+
+    h = total / 3600;
+    m = (total % 3600) / 60;
+    s = total % 60;
+
+    printf("time = %d:%d:%d\n", h, m, s);
+
+    return 0;
+}
