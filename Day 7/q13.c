f482605b70b7bdf130dd0ef7b7c13feabd59fb79
Kanishk-Rana <kanishk.33995@stu.upes.ac.in>
2026-08-16 01:00:00 +0530

    Day 7

---
+++ Day 7/q13.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int year;
+
+    printf("enter year: ");
+    scanf("%d", &year);
+
+    if(year % 400 == 0)
+    {
+        printf("leap year\n");
+    }
+    else if(year % 100 == 0)
+    {
+        printf("not a leap year\n");
+    }
+    else if(year % 4 == 0)
+    {
+        printf("leap year\n");
+    }
+    else
+    {
+        printf("not a leap year\n");
+    }
+
+    return 0;
+}
