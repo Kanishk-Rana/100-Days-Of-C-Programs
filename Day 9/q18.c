f25bb0081fb7034cd1993eb0c562aff6cdb74327
Kanishk-Rana <kanishk.33995@stu.upes.ac.in>
2026-08-18 01:00:00 +0530

    Day 9

---
+++ Day 9/q18.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    float p;
+
+    printf("enter percentage: ");
+    scanf("%f", &p);
+
+    if(p >= 90 && p <= 100)
+        printf("grade a\n");
+    else if(p >= 80)
+        printf("grade b\n");
+    else if(p >= 70)
+        printf("grade c\n");
+    else if(p >= 60)
+        printf("grade d\n");
+    else if(p >= 0)
+        printf("grade f\n");
+    else
+        printf("invalid percentage\n");
+
+    return 0;
+}
