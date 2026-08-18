93018af8506b4202ac1ab7b8e735adf03492a9d5
Kanishk-Rana <kanishk.33995@stu.upes.ac.in>
2026-08-19 01:00:00 +0530

    Day 10

---
+++ Day 10/q19.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    printf("enter three sides: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a == b && b == c)
+        printf("equilateral triangle\n");
+    else if(a == b || b == c || a == c)
+        printf("isosceles triangle\n");
+    else
+        printf("scalene triangle\n");
+
+    return 0;
+}
---
+++ Day 10/q20.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("enter day number: ");
+    scanf("%d", &n);
+
+    switch(n)
+    {
+        case 1:
+            printf("monday\n");
+            break;
+        case 2:
+            printf("tuesday\n");
+            break;
+        case 3:
+            printf("wednesday\n");
+            break;
+        case 4:
+            printf("thursday\n");
+            break;
+        case 5:
+            printf("friday\n");
+            break;
+        case 6:
+            printf("saturday\n");
+            break;
+        case 7:
+            printf("sunday\n");
+            break;
+        default:
+            printf("invalid day number\n");
+    }
+
+    return 0;
+}
