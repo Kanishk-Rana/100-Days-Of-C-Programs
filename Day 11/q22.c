7d37908a3faa7551228fc46861f8e5c7631ae5a5
Kanishk-Rana <kanishk.33995@stu.upes.ac.in>
2026-08-20 01:00:00 +0530

    Day 11

---
+++ Day 11/q22.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    float cp, sp, p;
+
+    printf("enter cost price: ");
+    scanf("%f", &cp);
+
+    printf("enter selling price: ");
+    scanf("%f", &sp);
+
+    if(sp > cp)
+    {
+        p = ((sp - cp) / cp) * 100;
+        printf("profit percentage = %.2f\n", p);
+    }
+    else if(cp > sp)
+    {
+        p = ((cp - sp) / cp) * 100;
+        printf("loss percentage = %.2f\n", p);
+    }
+    else
+    {
+        printf("no profit no loss\n");
+    }
+
+    return 0;
+}
