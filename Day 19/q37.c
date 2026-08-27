838b826437b194345a04fab0f3a060cf77abf608
Kanishk-Rana <kanishk.33995@stu.upes.ac.in>
2026-08-28 01:00:00 +0530

    Day 19

---
+++ Day 19/q37.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, max, lcm;
+
+    printf("enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if(a > b)
+        max = a;
+    else
+        max = b;
+
+    while(1)
+    {
+        if(max % a == 0 && max % b == 0)
+        {
+            lcm = max;
+            break;
+        }
+
+        max++;
+    }
+
+    printf("lcm = %d\n", lcm);
+
+    return 0;
+}
