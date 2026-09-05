3e95432c2682be375331f5f81bba6fa4c8d4227a
Kanishk-Rana <kanishk.33995@stu.upes.ac.in>
2026-09-06 01:00:00 +0530

    Day 28

---
+++ Day 28/q56.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i;
+
+    printf("enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("enter elements: ");
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("array elements are: ");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
