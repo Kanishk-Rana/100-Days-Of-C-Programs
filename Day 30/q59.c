f7254fd73ee13b2665fe6e30e71711956ffcce91
Kanishk-Rana <kanishk.33995@stu.upes.ac.in>
2026-09-08 01:00:00 +0530

    Day 30

---
+++ Day 30/q59.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, even = 0, odd = 0;
+
+    printf("enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("enter elements: ");
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        if(a[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("even numbers = %d\n", even);
+    printf("odd numbers = %d\n", odd);
+
+    return 0;
+}
