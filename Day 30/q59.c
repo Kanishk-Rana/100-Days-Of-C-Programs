08b216cf75ea9a758dfb010623d4a630ebc6f9f0
Kanishk-Rana <kanishk.33995@stu.upes.ac.in>
2026-09-08 06:43:33 +0530

    Day 30

---
+++ Day 30/q59.c
@@ -0,0 +1,30 @@
+//Count even and odd numbers in an array.
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, even = 0, odd = 0;
+
+    printf("enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("enter elements: ");
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        if(a[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("even numbers = %d\n", even);
+    printf("odd numbers = %d\n", odd);
+
+    return 0;
+}
---
+++ Day 30/q60.c
@@ -0,0 +1,34 @@
+//Count positive, negative, and zero elements in an array.
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i;
+    int positive = 0, negative = 0, zero = 0;
+
+    printf("enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("enter elements: ");
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        if(a[i] > 0)
+            positive++;
+        else if(a[i] < 0)
+            negative++;
+        else
+            zero++;
+    }
+
+    printf("positive = %d\n", positive);
+    printf("negative = %d\n", negative);
+    printf("zero = %d\n", zero);
+
+    return 0;
+}
