1153479086a855c7cdbe5c73d2ea2c8ffea398a3
Kanishk-Rana <kanishk.33995@stu.upes.ac.in>
2026-09-13 01:00:00 +0530

    Day 35

---
+++ Day 35/q69.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, largest, second;
+
+    printf("enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("enter elements: ");
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    largest = a[0];
+    second = a[1];
+
+    if(second > largest)
+    {
+        int temp = largest;
+        largest = second;
+        second = temp;
+    }
+
+    for(i = 2; i < n; i++)
+    {
+        if(a[i] > largest)
+        {
+            second = largest;
+            largest = a[i];
+        }
+        else if(a[i] > second && a[i] != largest)
+        {
+            second = a[i];
+        }
+    }
+
+    printf("second largest element = %d\n", second);
+
+    return 0;
+}
---
+++ Day 35/q70.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, k, i, j, temp;
+
+    printf("enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("enter elements: ");
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("enter number of rotations: ");
+    scanf("%d", &k);
+
+    k = k % n;
+
+    for(i =0; i<k; i++)
+    {
+        temp = a[n - 1];
+
+        for(j = n - 1; j > 0; j--)
+        {
+            a[j] = a[j - 1];
+        }
+
+        a[0] = temp;
+    }
+
+    printf("array after rotation: ");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
